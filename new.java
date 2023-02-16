0ef04daf0019fb6f1094e1527fc291c1f77179cf
end_aan <md.mollick15697@sakec.ac.in>
2023-02-16 13:26:49 +0530

    Update new.java

--- new.java
+++ new.java
@@ -1,5 +1,5 @@
-class new{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-} 
+public class new {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
